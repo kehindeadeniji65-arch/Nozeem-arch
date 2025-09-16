f3caa2c2de556109a35e7ecedad21cad7578f481
Nozeem <kehindeadeniji65@gmail.com>
2025-09-16 14:49:19 +0100

    remodified

---
+++ BABWEB.js
@@ -0,0 +1,8 @@
+function showBar(){
+    let leftbar = document.querySelector('.leftbar')
+    leftbar.style.display='flex'
+}
+function hideBar(){
+    let leftbar = document.querySelector('.leftbar')
+    leftbar.style.display='none'
+}
